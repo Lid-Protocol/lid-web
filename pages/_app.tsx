cbfe07e9012f92193a3f078a9d99d4b25457218e
Jikun Liao <liaojikundever@gmail.com>
2020-09-14 03:59:15 +0300

    Add metadata

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { NextComponentType } from 'next';
 import { AppContext, AppInitialProps, AppProps } from 'next/app';
 import { ThemeProvider } from 'styled-components';
+import Head from 'next/head';
 
 import AppWrapper from 'components/AppWrapper';
 import TopBar from 'components/TopBar';
@@ -10,9 +11,32 @@ import theme from 'styles/theme';
 import 'styles/basic.css';
 import 'styles/fonts.css';
 
-const App: NextComponentType<AppContext, AppInitialProps, AppProps> = ({ Component, pageProps }) => (
+const App: NextComponentType<AppContext, AppInitialProps, AppProps> = ({
+  Component,
+  pageProps,
+}) => (
   <ThemeProvider theme={theme}>
     <AppWrapper>
+      <Head>
+        <meta itemProp="image" content="/static/opengraph.png" />
+        <meta property="og:url" content="https://lid.sh" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Liquidity Dividends Protocol" />
+        <meta
+          property="og:description"
+          content="Liquidity Dividends Protocol"
+        />
+        <meta property="og:image" content="/static/opengraph.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Liquidity Dividends Protocol" />
+        <meta
+          name="twitter:description"
+          content="Liquidity Dividends Protocol"
+        />
+        <meta name="twitter:image" content="/static/opengraph.png" />
+        <link rel="icon" type="image/png" href="/static/logo.png" />
+        <link rel="apple-touch-icon" href="/static/logo.png" />
+      </Head>
       <TopBar />
       <Component {...pageProps} />
       <Footer />
