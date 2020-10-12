cf6d6a61d17cf9dc15488e0d964cc168175042a4
Kyle Longrich <longrichk@gmail.com>
2020-10-12 03:01:01 -0400

    index updates

--- pages/blog.js
+++ pages/blog.js
@@ -6,17 +6,47 @@ import DeFiArticle from './DeFiArticle'
 
 import front_image from './splash.jpeg'
 
-import {BlogData} from './blogPostBackUp'
-
 //Stored this info on IPFS
-const BlogPostData = [ 
+const BlogData = [
     {
-        id: '',
-        picture: front_image,
-        title: "",
-        content: "",
-        readtime: "",
-        ipfsHash: ""
+        id : '1',
+        picture : "front_image",
+        title : "6 Reasons why DEFI is here to stay and not a fad!",
+        content : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        readtime : "Oct 5, 2020 13 min read",
+        ipfsHash : ""
+    },
+    {
+        id : '2',
+        picture : "front_image",
+        title : "6 Reasons why DEFI is here to stay and not a fad!",
+        content : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        readtime : "Oct 5, 2020 13 min read",
+        ipfsHash : ""
+    },
+    {
+        id : '3',
+        picture : "front_image",
+        title : "6 Reasons why DEFI is here to stay and not a fad!",
+        content : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        readtime : "Oct 5, 2020 13 min read",
+        ipfsHash : ""
+    },
+    {
+        id : '4',
+        picture : "front_image",
+        title : "6 Reasons why DEFI is here to stay and not a fad!",
+        content : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        readtime : "Oct 5, 2020 13 min read",
+        ipfsHash : ""
+    },
+    {
+        id : '5',
+        picture : "front_image",
+        title : "6 Reasons why DEFI is here to stay and not a fad!",
+        content : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        readtime : "Oct 5, 2020 13 min read",
+        ipfsHash : ""
     }
 ]
 
@@ -70,7 +100,7 @@ const BlogPostBox = styled.div`
 export default function Blog () {
 
     const [veiwingPage, setVeiwingPage] = useState(false);
-    const [ListOfPost, setListOfPost] = useState(BlogPostData);
+    const [ListOfPost, setListOfPost] = useState(BlogData);
     const [dataLoaded, setDataloaded] = useState(false);
 
     async function get_list_of_post() {
--- pages/index.tsx
+++ pages/index.tsx
@@ -13,37 +13,6 @@ import {
 import Partners from 'components/@pages/Home/Partners';
 
 export default function Home (): any {
-  
-  const [isBlog, setIsBlog] = useState(false);
-
-  function check_blogPage() {
-    var Temp = window.location.href.split("#");
-
-    if (Temp[1] == "blog"){
-      setIsBlog(true);
-    }
-
-    console.log(Temp[1])
-  }
-
-  useEffect(() => {
-    check_blogPage();
-  })
-
-  if (isBlog) {
-    return (
-      <>
-        <Head>
-          <title>Liquidity Dividends Protocol</title>
-        </Head>
-
-        <p>
-          Hello World
-        </p>
-
-      </>
-    )
-  } else {
     return (
       <>
         <Head>
@@ -60,6 +29,6 @@ export default function Home (): any {
         <Legal />
       </>
     )
-  }
 }
+
 
