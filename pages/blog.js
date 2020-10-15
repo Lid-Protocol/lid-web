c0c987eecfe52ffd8f2b88435da468730293c1ce
Kyle Longrich <longrichk@gmail.com>
2020-10-15 00:52:31 -0400

    removing duplicate post

--- pages/blog.js
+++ pages/blog.js
@@ -13,38 +13,6 @@ const BlogData = [
         content : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         readtime : "Oct 5, 2020 13 min read",
         ipfsHash : "QmUPXLrbCyMryPhnKfanj5pNhu9PkxQPMBK21tNCXzH218"
-    },
-    {
-        id : '2',
-        picture : "https://ipfs.io/ipfs/QmW7AzrgnpAEB2mW8i4Piozu6R38kdS6DAPkifarkTWLd2",
-        title : "6 Reasons why DEFI is here to stay and not a fad!",
-        content : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        readtime : "Oct 5, 2020 13 min read",
-        ipfsHash : ""
-    },
-    {
-        id : '3',
-        picture : "https://ipfs.io/ipfs/QmW7AzrgnpAEB2mW8i4Piozu6R38kdS6DAPkifarkTWLd2",
-        title : "6 Reasons why DEFI is here to stay and not a fad!",
-        content : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        readtime : "Oct 5, 2020 13 min read",
-        ipfsHash : ""
-    },
-    {
-        id : '4',
-        picture : "https://ipfs.io/ipfs/QmW7AzrgnpAEB2mW8i4Piozu6R38kdS6DAPkifarkTWLd2",
-        title : "6 Reasons why DEFI is here to stay and not a fad!",
-        content : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        readtime : "Oct 5, 2020 13 min read",
-        ipfsHash : ""
-    },
-    {
-        id : '5',
-        picture : "https://ipfs.io/ipfs/QmW7AzrgnpAEB2mW8i4Piozu6R38kdS6DAPkifarkTWLd2",
-        title : "6 Reasons why DEFI is here to stay and not a fad!",
-        content : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nis dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-        readtime : "Oct 5, 2020 13 min read",
-        ipfsHash : ""
     }
 ]
 
