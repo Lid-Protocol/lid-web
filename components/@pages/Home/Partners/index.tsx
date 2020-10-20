c09d8b1d50f7ce7c7cb422be2d8f7810bbac32c3
Kyle Longrich <longrichk@gmail.com>
2020-10-20 18:35:16 -0400

    removing xfi partner

--- components/@pages/Home/Partners/index.tsx
+++ components/@pages/Home/Partners/index.tsx
@@ -9,14 +9,10 @@ import AskboBarImage from './AskboBar.png';
 import DEXToolsImage from './DEXTools.png';
 import XFIImage from './XFI.png';
 import QuillAuditImage from './QuillAudits.png';
-import TUSDImage from './TUSD.png'
+import TUSDImage from './TUSD.png';
 
 const Partners: React.FC = () => (
-  <Box
-    id="partners"
-    background={colors.lightBlueGradient}
-    py={5}
-  >
+  <Box id="partners" background={colors.lightBlueGradient} py={5}>
     <ContentWrapper pt={4}>
       <FlexBox flexDirection="column" alignItems="center" flexWrap="wrap">
         <H2 marginBottom={4}>Partners</H2>
@@ -27,29 +23,24 @@ const Partners: React.FC = () => (
           mt={[0, 5.5]}
         >
           <a href="https://askobar-network.com/">
-          <Box maxWidth={['300px', '300px', '300px', '300px']} mb="2rem">
-            <Image width="70%" height="auto" src={AskboBarImage} />
-          </Box>
+            <Box maxWidth={['300px', '300px', '300px', '300px']} mb="2rem">
+              <Image width="70%" height="auto" src={AskboBarImage} />
+            </Box>
           </a>
           <a href="https://www.dextools.io/">
-          <Box maxWidth={['300px', '300px', '300px', '300px']} mb="2rem">
-            <Image width="70%" height="auto" src={DEXToolsImage} />
-          </Box>
-          </a>
-          <a href="https://xfinance.io/">
-          <Box maxWidth={['300px', '300px', '300px', '300px']} mb="2rem">
-            <Image width="70%" height="auto" src={XFIImage} />
-          </Box>
+            <Box maxWidth={['300px', '300px', '300px', '300px']} mb="2rem">
+              <Image width="70%" height="auto" src={DEXToolsImage} />
+            </Box>
           </a>
           <a href="https://audits.quillhash.com/">
-          <Box maxWidth={['300px', '300px', '300px', '300px']} mb="2rem">
-            <Image width="70%" height="auto" src={QuillAuditImage} />
-          </Box>
+            <Box maxWidth={['300px', '300px', '300px', '300px']} mb="2rem">
+              <Image width="70%" height="auto" src={QuillAuditImage} />
+            </Box>
           </a>
           <a href="https://www.trusttoken.com/">
-          <Box maxWidth={['300px', '300px', '300px', '300px']} mb="2rem">
-            <Image width="70%" height="auto" src={TUSDImage} />
-          </Box>
+            <Box maxWidth={['200px', '200px', '200px', '200px']} mb="2rem">
+              <Image width="70%" height="auto" src={TUSDImage} />
+            </Box>
           </a>
         </FlexBox>
       </FlexBox>
