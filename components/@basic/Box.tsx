9f1fad095ac3c9f7f0b424f61dcb7cdae1cb94fd
liaojikunwork <liaojikundever@gmail.com>
2020-11-05 06:34:35 -0600

    Feauter/term sheet (#21)
    * initial
    
    * Update Term Sheet Logic
    
    * Mobile UI, Fix typescript errors
    
    * Add form submission
    
    * Minor
    
    * remove package-lock.json

--- components/@basic/Box.tsx
+++ components/@basic/Box.tsx
@@ -1,11 +1,49 @@
 import { FC, CSSProperties, ReactNode } from 'react';
 import styled from 'styled-components';
-import { flexbox, fontSize, buttonStyle, space, layout, typography, color, background, display, border, size, boxShadow } from 'styled-system';
-import type { FlexboxProps, FontSizeProps, ButtonStyleProps, SpaceProps, LayoutProps, TypographyProps, ColorProps, BackgroundProps, DisplayProps, BorderProps, SizeProps, BoxShadowProps } from 'styled-system';
+import {
+  flexbox,
+  fontSize,
+  buttonStyle,
+  space,
+  layout,
+  typography,
+  color,
+  background,
+  display,
+  border,
+  size,
+  boxShadow,
+} from 'styled-system';
+import type {
+  FlexboxProps,
+  FontSizeProps,
+  ButtonStyleProps,
+  SpaceProps,
+  LayoutProps,
+  TypographyProps,
+  ColorProps,
+  BackgroundProps,
+  DisplayProps,
+  BorderProps,
+  SizeProps,
+  BoxShadowProps,
+} from 'styled-system';
 
-export type BoxProps =
-  { id?: string, children?: ReactNode, css?: string, src?: string, as?: string, style?: CSSProperties } &
-  FlexboxProps &
+export type BoxProps = {
+  id?: string;
+  children?: ReactNode;
+  css?: string;
+  src?: string;
+  as?: string;
+  style?: CSSProperties;
+  type?: string;
+  readOnly?: boolean;
+  disabled?: boolean;
+  value?: string | number;
+  defaultValue?: string | number;
+  onClick?: (e: Event) => void;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+} & FlexboxProps &
   SpaceProps &
   SizeProps &
   FontSizeProps &
--- components/@basic/index.tsx
+++ components/@basic/index.tsx
@@ -4,5 +4,6 @@ export { default as Image } from './Image';
 export { default as Text } from './Text';
 export { default as List } from './List';
 export { default as Button } from './Button';
+export { default as Input } from './Input';
 
 export type { BoxProps, BoxType } from './Box';
