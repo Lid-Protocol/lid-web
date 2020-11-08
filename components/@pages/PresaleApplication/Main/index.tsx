606071d10081e5d823bbc16424456970c0952ac1
Jikun Liao <liaojikundever@gmail.com>
2020-11-08 09:30:17 -0600

    Make as a separate page

--- components/@pages/PresaleApplication/Main/index.tsx
+++ components/@pages/PresaleApplication/Main/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useState } from 'react';
+import { FC } from 'react';
 
 import { Box, Text, Button, FlexBox, Image, List } from 'components/@basic';
 import ContentWrapper from 'components/ContentWrapper';
@@ -7,28 +7,13 @@ import H2 from 'components/H2';
 import H3 from 'components/H3';
 
 import { colors } from 'styles/theme';
-
-import SubmitForm from '../SubmitForm';
-
 import certifiedLabelImage from './certified-label.png';
 import LIDCertifiedLogoImage from './lid-certified-logo.png';
 
 const Header: FC = () => {
-  const [modalIsOpen, setIsOpen] = useState(false);
-
-  const openModal = () => {
-    setIsOpen(true);
-  };
-
-  const closeModal = () => {
-    setIsOpen(false);
-  };
-
   return (
     <>
       <ContentWrapper>
-        <SubmitForm isOpen={modalIsOpen} closeModal={closeModal} />
-
         <FlexBox
           flexDirection={['column-reverse', 'column-reverse', 'row']}
           mt={5}
@@ -78,8 +63,7 @@ const Header: FC = () => {
                 mr={4}
                 mb={[4, 0]}
                 alignSelf={['inherit', 'inherit', 'flex-start']}
-                onClick={openModal}
-                // href="mailto:team@lid.sh"
+                href="/termsheet"
               >
                 Submit Application
               </Button>
